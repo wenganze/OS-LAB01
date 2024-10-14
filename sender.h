4cd41f9643230e3de5976f9d646f1cd951ec3388
mephen <boy4561400@gmail.com>
2024-10-14 14:41:58 +0800

    update receiver.h & sender.h

--- sender.h
+++ sender.h
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#include <time.h>
 
 typedef struct {
     int flag;      // 1 for message passing, 2 for shared memory
